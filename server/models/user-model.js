2381b2d551293868d8f7291aa9301925820fb843
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-11-14 19:24:55 +0600

    #9

---
+++ server/models/user-model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = new model("User", userSchema);
+
+export default User;
