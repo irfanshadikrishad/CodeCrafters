741e2ea73a568261b75bed1c845aade43853c9a3
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-11-29 12:42:47 +0600

    #22 - registration page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,63 @@
+import { useState } from "react";
+
 export default function Register() {
-  return <section className="container">Register</section>;
+  const [register, setRegister] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setRegister({ ...register, [name]: value });
+  };
+  const submit = async (e) => {
+    e.preventDefault();
+    console.log(register);
+  };
+  return (
+    <section className="container register">
+      <div className="register_image">
+        <img src="/designer.svg" alt="register_image" draggable="false" />
+      </div>
+      <div className="register_form">
+        <h1>Register</h1>
+        <form onSubmit={submit}>
+          <input
+            name="username"
+            onChange={handleInput}
+            required
+            value={register.username}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            name="email"
+            onChange={handleInput}
+            required
+            value={register.email}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            name="phone"
+            onChange={handleInput}
+            required
+            value={register.phone}
+            type="phone"
+            placeholder="Phone"
+          />
+          <input
+            name="password"
+            onChange={handleInput}
+            required
+            value={register.password}
+            type="password"
+            placeholder="Password"
+          />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </section>
+  );
 }
