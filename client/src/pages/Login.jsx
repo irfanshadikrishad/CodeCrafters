e89ad23672abd5b44b987c98a9954ad829f3a4c9
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2023-11-30 05:01:44 +0600

    #23 - login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+
 export default function Login() {
-  return <section className="container">Login</section>;
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+  const submit = async (e) => {
+    e.preventDefault();
+    console.log(login);
+  };
+  return (
+    <section className="container register">
+      <div className="register_image">
+        <img src="/designer.svg" alt="register_image" draggable="false" />
+      </div>
+      <div className="register_form">
+        <h1>Login</h1>
+        <form onSubmit={submit}>
+          <input
+            name="email"
+            onChange={handleInput}
+            required
+            value={login.email}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            name="password"
+            onChange={handleInput}
+            required
+            value={login.password}
+            type="password"
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </section>
+  );
 }
